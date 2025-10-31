c319d7620bc2070dea46657cf127c8ac7fe0e44d
poudel-bibek <serpent.03.meshes@icloud.com>
2025-10-31 12:56:45 -0400

    minor

--- main.py
+++ main.py
@@ -620,7 +620,7 @@ def build_arg_parser() -> argparse.ArgumentParser:
     parser.add_argument("--stop_duration", type=int, default=60, help="Stop duration")
     parser.add_argument("--update_frequency", type=int, default=1024, help="Update PPO when memory has N samples")
     parser.add_argument("--num_episodes", type=int, default=2000, help="Total training episodes")
-    parser.add_argument("--eval_every", type=int, default=10, help="Evaluate every N updates to the policy")
+    parser.add_argument("--eval_every", type=int, default=2, help="Evaluate every N updates to the policy")
     parser.add_argument("--baseline_type", type=str, default="demand_cover", help="Can be random_walk, reward_max, demand_cover, shortest_path, real_world")
     parser.add_argument("--num_eval_runs", type=int, default=5, help="Number of runs (over which we average the results) for both evaluation and baselines")
     parser.add_argument("--eval_seed_offset", type=int, default=2, help="Add offset to starting seed for evaluation outputs")
--- sweep.py
+++ sweep.py
@@ -39,7 +39,7 @@ def build_sweep_config() -> Dict[str, Any]:
             # Some other params to be set as fixed values
             "gpu": {"value": True},
             "anneal_lr": {"value": True},
-            "num_episodes": {"value": 250},
+            "num_episodes": {"value": 300},
         },
     }
 
